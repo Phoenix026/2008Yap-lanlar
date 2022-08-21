26a5a434e9b5434b399bfbacf7864147fdda0c86
Berke BERBER <108575142+Phoenix026@users.noreply.github.com>
2022-08-21 15:28:54 +0300

    güncel

--- WinFormApp/WinFormApp/Form1.Designer.cs
+++ WinFormApp/WinFormApp/Form1.Designer.cs
@@ -35,74 +35,100 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(51, 44);
+            this.label1.Location = new System.Drawing.Point(64, 66);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(30, 16);
+            this.label1.Size = new System.Drawing.Size(42, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "Ad :";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(54, 80);
+            this.label2.Location = new System.Drawing.Point(68, 120);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 16);
+            this.label2.Size = new System.Drawing.Size(68, 24);
             this.label2.TabIndex = 1;
             this.label2.Text = "Soyad :";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(54, 116);
+            this.label3.Location = new System.Drawing.Point(68, 174);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(87, 16);
+            this.label3.Size = new System.Drawing.Size(118, 24);
             this.label3.TabIndex = 2;
             this.label3.Text = "TC Kimlik No:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(87, 44);
+            this.textBox1.Location = new System.Drawing.Point(109, 66);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(179, 22);
+            this.textBox1.Size = new System.Drawing.Size(223, 32);
             this.textBox1.TabIndex = 3;
             this.textBox1.Text = "Efe Berke";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(113, 80);
+            this.textBox2.Location = new System.Drawing.Point(141, 120);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(154, 22);
+            this.textBox2.Size = new System.Drawing.Size(192, 32);
             this.textBox2.TabIndex = 4;
             this.textBox2.Text = "Berber";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(148, 116);
+            this.textBox3.Location = new System.Drawing.Point(185, 174);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(119, 22);
+            this.textBox3.Size = new System.Drawing.Size(148, 32);
             this.textBox3.TabIndex = 5;
             this.textBox3.Text = "10418234141";
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(192, 153);
+            this.button1.Location = new System.Drawing.Point(240, 230);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(94, 34);
             this.button1.TabIndex = 6;
             this.button1.Text = "Giriş Yap";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(68, 311);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 7;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(390, 486);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(262, 32);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Aquamarine;
+            this.ClientSize = new System.Drawing.Size(1000, 675);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.monthCalendar1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
@@ -110,6 +136,9 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.Color.Navy;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -126,6 +155,8 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
 
--- WinFormApp/WinFormApp/Form1.cs
+++ WinFormApp/WinFormApp/Form1.cs
@@ -18,43 +18,51 @@ namespace WinFormApp
             //TEXTBOXLAR
 
             TextBox tb_Selam = new TextBox();
+            
             tb_Selam.Size = new Size(130, 60);
-            tb_Selam.Location = new Point(280, 35);
+            tb_Selam.Location = new Point(460, 35);
             this.Controls.Add(tb_Selam);
 
             TextBox tb_Selamın = new TextBox();
             tb_Selamın.Size = new Size(130, 55);
-            tb_Selamın.Location = new Point(300, 65);
+            tb_Selamın.Location = new Point(460, 65);
             this.Controls.Add(tb_Selamın);
 
             TextBox tb_Sa = new TextBox();
             tb_Sa.Size = new Size(130, 60);
-            tb_Sa.Location = new Point(340, 95);
+            tb_Sa.Location = new Point(460, 95);
             this.Controls.Add(tb_Sa);
 
             //LABELLER
 
+            this.label1.ForeColor = System.Drawing.Color.DarkGreen;
+
             Label lbl_Selam = new Label();
             lbl_Selam.Size = new Size(100, 20);
-            lbl_Selam.Location = new Point(260, 35);
+            lbl_Selam.Location = new Point(430, 35);
             lbl_Selam.Text = "Ad:";
             this.Controls.Add(lbl_Selam);
 
             Label lbl_Aleyküm = new Label();
             lbl_Aleyküm.Size = new Size(100, 20);
-            lbl_Aleyküm.Location = new Point(260, 65);
+            lbl_Aleyküm.Location = new Point(400, 65);
             lbl_Aleyküm.Text = "Soyad:";
             this.Controls.Add(lbl_Aleyküm);
             
             Label lbl_As = new Label();
             lbl_As.Size = new Size(100, 20);
-            lbl_As.Location = new Point(260, 95);
+            lbl_As.Location = new Point(365, 95);
             lbl_As.Text = "TC Kimlik No:";
             this.Controls.Add(lbl_As);
 
         }
 
         private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
         {
 
         }
